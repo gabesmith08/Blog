4477e8487ae7a7493789ccae884006b3ee94fc7f
Gabe Smith <gabe.smith@coop.org>
2022-03-02 16:43:30 -0600

    css changes

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                ></textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
