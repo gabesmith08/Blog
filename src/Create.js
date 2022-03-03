ceb75bf9ec3c21e0fbabe855f9286b26434ff6ac
Gabe Smith <gabe.smith@coop.org>
2022-03-03 16:23:20 -0600

    added dynamic values to Create.js

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -7,17 +13,25 @@ const Create = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog Body:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog Author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{author}</p>
             </form>
         </div>
      );
