1c2dffaca7e537d63be96e3036c64712bcf6bb5e
Gabe Smith <gabe.smith@coop.org>
2022-03-07 14:03:04 -0600

    added new delete button on blog details

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
-    const [isPending, setIsPending] = useState(false)
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,8 +20,11 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(() => {
             console.log('new blog added');
-            isPending(false);
+            setIsPending(false);
+            // history.go(-1); takes you back to previous page, regardless of where you are in the app.
+            history.push('/');
         })
+
     }
 
     return ( 
